c906582bf31e7fb102665d2341b5de4268f84e73
Konstantinos Rematas <krematas@cs.washington.edu>
2018-09-05 17:46:04 -0700

    ball saving and annotation
    Signed-off-by: Konstantinos Rematas <krematas@cs.washington.edu>

--- demo/get_ball.py
+++ demo/get_ball.py
@@ -1,10 +1,10 @@
 import argparse
 import soccer3d
 import numpy as np
-
+from os.path import join
 
 parser = argparse.ArgumentParser(description='Calibrate a soccer video')
-parser.add_argument('--path_to_data', default='/home/krematas/Mountpoints/grail/data/barcelona', help='path')
+parser.add_argument('--path_to_data', default='/home/krematas/Mountpoints/grail/data/Singleview/Soccer/Netherlands-Japan-0', help='path')
 parser.add_argument('--openpose_dir', default='/home/krematas/code/openpose', help='path')
 opt, _ = parser.parse_known_args()
 
@@ -21,5 +21,6 @@ db.get_ball_from_detectron(thresh=0.8)
 path_to_save = db.path_to_dataset + '/metadata/ball.npy'
 np.save(path_to_save, db.ball)
 
+np.save(join(db.path_to_dataset, 'ball', 'poses.npy'), db.poses)
 
 db.dump_video('detections')
