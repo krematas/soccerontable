fb2f689c74a8ed72cefe219183a7962e879945cf
krematas <krematas@gmail.com>
2018-10-13 20:25:42 -0700

    add padding as parameter for the cropped images
    Signed-off-by: krematas <krematas@gmail.com>

--- demo/estimate_poses.py
+++ demo/estimate_poses.py
@@ -5,6 +5,7 @@ import soccer3d
 parser = argparse.ArgumentParser(description='Calibrate a soccer video')
 parser.add_argument('--path_to_data', default='/home/krematas/Mountpoints/grail/data/barcelona', help='path')
 parser.add_argument('--openpose_dir', default='/home/krematas/code/openpose', help='path')
+parser.add_argument('--pad', type=int, default=150, help='pad around players')
 opt, _ = parser.parse_known_args()
 
 
@@ -16,6 +17,6 @@ db.digest_metadata()
 db.get_boxes_from_detectron()
 # db.dump_video('detections')
 
-db.estimate_poses(openpose_dir=opt.openpose_dir)
+db.estimate_poses(openpose_dir=opt.openpose_dir, pad=opt.pad)
 db.refine_poses(keypoint_thresh=7, score_thresh=0.4, neck_thresh=0.4)
 db.dump_video('poses')
